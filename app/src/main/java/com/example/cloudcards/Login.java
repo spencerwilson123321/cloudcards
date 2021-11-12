e825fb01a8af99e659ab0c64dcf3947b3a1f2187
spencerwilson123321 <swilson176@my.bcit.ca>
2021-11-12 12:37:55 -0800

    full login and registration functionality finished using sqlite

--- app/src/main/java/com/example/cloudcards/Login.java
+++ app/src/main/java/com/example/cloudcards/Login.java
@@ -5,13 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.cloudcards.database.DBHelper;
 
 public class Login extends AppCompatActivity {
+
+    EditText email, password;
+    Button sign_in;
+    DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        email = (EditText) findViewById(R.id.login_email);
+        password = (EditText) findViewById(R.id.login_password);
+        sign_in = (Button) findViewById(R.id.login_button);
+        db = new DBHelper(this);
+
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailText = email.getText().toString();
+                String pass = password.getText().toString();
+                if (emailText.equals("") || pass.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    Boolean checkEmailPassword = db.checkEmailPassword(emailText, pass);
+                    if (checkEmailPassword == true) {
+                        Toast.makeText(getApplicationContext(), "Sign in success", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), Homepage.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Invalid Email and Password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
     }
 
     /**
--- app/src/main/java/com/example/cloudcards/Register.java
+++ app/src/main/java/com/example/cloudcards/Register.java
@@ -58,7 +58,6 @@ public class Register extends AppCompatActivity {
             }
         });
 
-
     }
 
     /**
