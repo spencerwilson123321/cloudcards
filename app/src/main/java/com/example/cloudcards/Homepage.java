a5209aab4282fc1c086070b0c46be256de944b31
spencerwilson123321 <spencerwilson123321@gmail.com>
2021-11-14 15:35:07 -0800

    basic text recognition working, shows results on a toast message for now

--- app/src/main/java/com/example/cloudcards/Homepage.java
+++ app/src/main/java/com/example/cloudcards/Homepage.java
@@ -1,15 +1,25 @@
 package com.example.cloudcards;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.SparseArray;
 import android.view.ContextThemeWrapper;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,6 +30,12 @@ import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.File;
 import java.io.IOException;
@@ -36,10 +52,21 @@ public class Homepage extends AppCompatActivity  implements MenuItem.OnMenuItemC
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int SEARCH_ACTIVITY_REQUEST_CODE = 2;
 
+    ImageView image_preview;
+    Uri image_uri;
+    String cameraPermission[];
+    String storagePermission[];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+
+        // Getting image preview
+        image_preview = findViewById(R.id.image_preview);
+        // permissions
+        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
         showMenu = (Button) findViewById(R.id.show_dropdown_menu);
         showMenu.setOnClickListener(new View.OnClickListener() {
@@ -70,22 +97,39 @@ public class Homepage extends AppCompatActivity  implements MenuItem.OnMenuItemC
 
     }
 
+    private void pickCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "NewPic");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Image to text");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        // Create and start the camera intent.
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        this.startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
     public void takePhoto(MenuItem menuItem) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(this.getPackageManager()) != null) {
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                //Uri photoURI = FileProvider.getUriForFile(this,"com.example.cloudcards",photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,mCurrentPhotoPath);
-                this.startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
+        if(!checkCameraPermissions()){
+            requestCameraPermission();
+        } else {
+            pickCamera();
         }
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(this.getPackageManager()) != null) {
+//            File photoFile = null;
+//            try {
+//                photoFile = createImageFile();
+//            } catch (IOException ex) {
+//                // Error occurred while creating the File
+//            }
+//            // Continue only if the File was successfully created
+//            if (photoFile != null) {
+//                //Uri photoURI = FileProvider.getUriForFile(this,"com.example.cloudcards",photoFile);
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,mCurrentPhotoPath);
+//                this.startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//            }
+//        }
     }
 
     public void startCollectionActivity(MenuItem menuItem) {
@@ -105,20 +149,83 @@ public class Homepage extends AppCompatActivity  implements MenuItem.OnMenuItemC
         return image;
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == SEARCH_ACTIVITY_REQUEST_CODE) {
 
         }
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            ImageView mImageView = (ImageView) findViewById(R.id.imageView);
-            mImageView.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
+            CropImage.activity(image_uri).setGuidelines(CropImageView.Guidelines.ON).start(this);
+//            ImageView mImageView = (ImageView) findViewById(R.id.imageView);
+//            mImageView.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
             //photos = presenter.findPhotos(new Date(Long.MIN_VALUE), new Date(), "", "", "");
         }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                // set image to imageview
+                image_preview.setImageURI(resultUri);
+                // get drawable bitmap for text recognition
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) image_preview.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+                if (!recognizer.isOperational()) {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                } else {
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<TextBlock> items = recognizer.detect(frame);
+                    StringBuilder sb = new StringBuilder();
+                    // Get text from StringBuilder until there is no text.
+                    for (int i = 0; i < items.size(); i++) {
+                        TextBlock myItem = items.valueAt(i);
+                        sb.append(myItem.getValue());
+                        sb.append("\n");
+                    }
+                    Toast.makeText(this, sb.toString(), Toast.LENGTH_LONG).show();
+                }
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
+                Exception error = result.getError();
+                Toast.makeText(this, ""+error, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, cameraPermission, 200);
+    }
+
+    private boolean checkStoragePermissions() {
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private boolean checkCameraPermissions() {
+        boolean result1 = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+        boolean result2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result1 && result2;
     }
 
     @Override
     public boolean onMenuItemClick(MenuItem menuItem) {
         return false;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 200:
+                if (grantResults.length > 0) {
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted && writeStorageAccepted) {
+                        pickCamera();
+                    } else {
+                        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        }
+    }
+
 }
