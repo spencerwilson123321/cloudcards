30b978b6ce3ea3f44b1c149dfec5fbd23535d5db
spencerwilson123321 <spencerwilson123321@gmail.com>
2021-11-27 11:15:13 -0800

    Refactored Login to MVP

--- app/src/main/java/com/example/cloudcards/Presenter/LoginActivityPresenter.java
+++ app/src/main/java/com/example/cloudcards/Presenter/LoginActivityPresenter.java
@@ -1,4 +1,33 @@
 package com.example.cloudcards.Presenter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.example.cloudcards.Model.DBHelper;
+import com.example.cloudcards.View.HomepageActivity;
+
 public class LoginActivityPresenter {
+
+    Context loginContext;
+    DBHelper DB;
+
+    public LoginActivityPresenter(Context context) {
+        loginContext = context;
+        DB = new DBHelper(loginContext);
+    }
+
+    public void signIn(String email, String password) {
+        Boolean checkEmailPassword = DB.checkEmailPassword(email, password);
+        if (checkEmailPassword == true) {
+            Toast.makeText(loginContext, "Sign in success", Toast.LENGTH_SHORT).show();
+            int userID = DB.getUserID(email, password);
+            Intent intent = new Intent(loginContext, HomepageActivity.class);
+            intent.putExtra("userID", userID);
+            loginContext.startActivity(intent);
+        } else {
+            Toast.makeText(loginContext, "Invalid Email and Password", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
--- app/src/main/java/com/example/cloudcards/View/LoginActivity.java
+++ app/src/main/java/com/example/cloudcards/View/LoginActivity.java
@@ -1,4 +1,59 @@
 package com.example.cloudcards.View;
 
-public class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.cloudcards.Presenter.LoginActivityPresenter;
+import com.example.cloudcards.R;
+import com.example.cloudcards.Register;
+import com.example.cloudcards.View.HomepageActivity;
+import com.example.cloudcards.Model.DBHelper;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button sign_in;
+    DBHelper db;
+    LoginActivityPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+//        getApplicationContext().deleteDatabase("cloudcards.db");
+        presenter = new LoginActivityPresenter(getApplicationContext());
+        email = (EditText) findViewById(R.id.login_email);
+        password = (EditText) findViewById(R.id.login_password);
+        sign_in = (Button) findViewById(R.id.login_button);
+        db = new DBHelper(this);
+
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailText = email.getText().toString();
+                String pass = password.getText().toString();
+                if (emailText.equals("") || pass.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    presenter.signIn(emailText, pass);
+                }
+            }
+        });
+    }
+
+    /**
+     * Switch to Register Activity.
+     * @param view
+     */
+    public void register(View view) {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+
 }
